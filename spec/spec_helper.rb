428d5f1d2a51a0b515444a7b485e2d6aeb730f71
Rafael Camargo <hello@rafaelcamargo.com>
2022-06-21 23:04:54 -0300

    chore: improve ci config
    setup coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@ require 'simplecov'
 require 'simplecov-html'
 require 'simplecov-lcov'
 
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.report_with_single_file = true
+  c.single_report_path = 'coverage/lcov.info'
+end
 SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
   SimpleCov::Formatter::LcovFormatter
